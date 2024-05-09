4d0616be6736eadcfe04bb561627869d39e4b9b3
Marina Costa <mcsantos28@outlook.com>
2024-05-09 17:49:28 -0300

    Implementa Segmented Control e calculo da idade e espacamentos

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -14,28 +14,58 @@ struct ContentView: View {
     //declarar a var como opcional faz com que se inicie com o campo do text field limpo
     @State var months: Int? = nil
     @State var result: Int?
+    let portes = ["Pequeno", "Médio", "Grande"]
+    @State var porte: String = "Pequeno"
     
     var body: some View {
         VStack (alignment: .leading){
+            
+//            Divider()
+            //insere uma linha de divisão (gestalt)
+            
+            Spacer()
             Text("Qual a idade do seu cão?")
                 .font(.system(size: 24))
                 //estabelece o tamanho da fonte do título
+            
             Text("Anos")
             TextField("Digite quantos anos completos seu cão tem...", value: $years, format: .number)
             
-                Text("Meses")
+            Text("Meses")
             TextField("E quantos meses, além disso, ele tem.", value: $months, format: .number)
             
-                Text("Porte")
-                //segmented control
+            Text("Porte")
+            Picker("Porte", selection: $porte) {
+                ForEach(portes, id: \.self) { porte in
+                    Text(porte)
+                        .tag(porte)
+                    // a tag serve para que o porte selecionado apareça no lugar do array de portes
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            
+            Divider()
+                .background(.gray)
+            Spacer()
             
             if let result {
                 EmptyView()
                 Text("Seu cachorro tem, em idade humana...")
+                    .foregroundColor(Color.blue)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    //centraliza o texto
+                    .font(.system(size: 20))
+                    .padding(.bottom, 20)
+                
                 Text("\(result) anos")
                     .font(.system(size: 45))
+                    .foregroundColor(Color.blue)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
 
-            } else {
+
+            } 
+            else {
                 Image(ImageResource.clarinha)
                     .resizable()
                     .scaledToFit()
@@ -46,12 +76,15 @@ struct ContentView: View {
                     //especifiquei que a largura pode ser máxima dada a escala
                     .shadow(radius: 20)
             }
+
             
+            Spacer()
             Button(action: {
-                result = 23
+                //comportamento do botão
+                processYears()
             }, label: {
                 ZStack {
-                    Color.pink
+                    Color.purple
                     Text("Cãocular")
                         .foregroundStyle(.white)
                         //mudando a cor do texto do botão
@@ -67,6 +100,30 @@ struct ContentView: View {
         .bold()
         .fontDesign(.rounded)
         .padding()
+    }
+    
+    func processYears() {
+        
+        guard let years,
+              let months
+        else {
+            print("Preencha o campo de entrada")
+            return
+            //o guard exige um return, mesmo que vazio
+        }
+        //guard é outra maneira de desempacotar a variável opcional
+        
+        guard years > 0 || months > 0 else {
+            print("Algum campo tem que ter valor maior que zero")
+            return
+        }
+        
+        result = years * 7 + months*7/12
+        
+//        if let years{
+//            result = 23
+//        }
+        //uma maneira de desempacotar a variável opcional
     }
 }
 
